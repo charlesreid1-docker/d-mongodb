3430903e93ae14f15afb69986d116c9350c99396
Charles Reid <charlesreid1@gmail.com>
2018-02-03 10:07:14 -0800

    Fixing/updating insert/read test scripts

--- test/test_mongo_insert.py
+++ test/test_mongo_insert.py
@@ -1,35 +1,19 @@
 from pymongo import MongoClient
 import random
+from datetime import datetime
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('10.6.0.1', 27017)
 db = client.test_database
 collection = db.test_collection
 
-docs = [{ 
-            'timestamp' : '2017-04-11 11:04:12',
-            'bssid' : 'DEADBEEFSFUOESIEWR', 
-            'channel' : random.randint(0,100), 
-            'ssid' : 'wiiiiiifi', 
-            'strength' : -24, 
-            'encryption' : 'WPA' 
-        },
-        {
-            'timestamp' : '2017-04-11 11:01:12',
-            'bssid' : 'BEEOIEUDSJFLKSDJFF', 
-            'channel' : random.randint(0,100), 
-            'ssid' : 'iurtiruyiuyrwe',
-            'strength' : -20, 
-            'encryption' : 'WPA' 
-        },
-        {
-            'timestamp' : '2017-04-11 11:24:12',
-            'bssid' : 'BEEF2DJFSKJFLKJSFKJL', 
-            'channel' : random.randint(0,100), 
-            'ssid' : 'sdfjaskfjlkdfjglfdghfketa', 
-            'strength' : -30, 
-            'encryption' : 'WPA' 
-        }]
+docs = []
+for i in range(20):
+    d = {
+            'timestamp' : datetime.now(),
+            'channel' : random.randint(1000,8000)
+        }
+    docs.append(d)
 
 result = collection.insert_many(docs)
-print result.inserted_ids
+print(result.inserted_ids)
 
